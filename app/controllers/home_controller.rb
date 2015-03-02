77177675ed245d94df153e80eb36de2f717ae14f
Kevin <kevinferm@gmail.com>
2015-03-02 18:05:49 +0100

    added admin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    ###Delete this later, adds everyone as admin!!
+    if current_user
+      user = User.find(current_user.id)
+      user.update({admin:true})
+      user.save!
+    end
   end
 end
