1de9e7b12a3163566e5b7529d8edcafeebb2aa61
Kevin <kevinferm@gmail.com>
2015-03-05 00:19:02 +0100

    Fixed deck building -- Looks horrible

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,9 +11,35 @@ class HomeController < ApplicationController
   def decks
     if current_user
       @user = User.find(current_user.id)
+      gon.decks = @user.decks
+      @cards = Card.all
     else
       redirect_to new_user_session_path
     end
+  end
+
+  def newdeck
+    if current_user
+      @user = User.find(current_user.id)
+      deck = deck_params[:deck].split(',')
+      decks = JSON.parse(@user.decks)
+      if not decks
+        decks = {}
+      end
+      decks[deck_params[:title]] = deck[0]
+      if @user.update({decks:decks.to_json})
+        render nothing:true
+        puts "Updated shizz plxxxxx"
+      end
+
+
+
+    end
+  end
+
+  private
+  def deck_params
+    params.require(:newdeck).permit!
   end
 
 end
